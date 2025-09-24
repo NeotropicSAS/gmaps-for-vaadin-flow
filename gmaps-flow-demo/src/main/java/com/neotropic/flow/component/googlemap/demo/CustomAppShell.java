77b3124917633f3726dc60ecbea34cf3fa5be04c
johny-ortega <johny.ortega@kuwaiba.org>
2025-09-24 14:50:00 -0500

    Initial commit: Snapshot of codebase from Subversion [(r7610)](https://sourceforge.net/p/kuwaiba/code/7610/tree/components/gmaps-flow/branches/version-24.6.1/gmaps-flow-root/)

---
+++ gmaps-flow-demo/src/main/java/com/neotropic/flow/component/googlemap/demo/CustomAppShell.java
@@ -0,0 +1,11 @@
+package com.neotropic.flow.component.googlemap.demo;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
+
+@Push
+@PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
+public class CustomAppShell implements AppShellConfigurator {
+    
+}
